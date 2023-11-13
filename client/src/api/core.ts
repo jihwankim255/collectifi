4fb43c5593834387d52caa142cc7c9675ff1d703
jhwan <plasticat104@gmail.com>
2023-11-14 00:38:32 +0900

    Add axiosInstance

---
+++ client/src/api/core.ts
@@ -0,0 +1,29 @@
+import axios, {AxiosInstance} from 'axios';
+
+export const api: AxiosInstance = axios.create({
+  baseURL: '/api',
+  timeout: 500000,
+});
+
+// 요청 전에 실행되는 인터셉터
+api.interceptors.request.use(
+  config => {
+    // 요청을 보내기 전에 수행할 작업 추가
+    return config;
+  },
+  error => {
+    // 요청 에러 처리
+    return Promise.reject(error);
+  },
+);
+// 응답을 받기 전에 실행되는 인터셉터
+api.interceptors.response.use(
+  response => {
+    // 응답 데이터를 가공하여 반환
+    return response.data;
+  },
+  error => {
+    // 응답 에러 처리
+    return Promise.reject(error);
+  },
+);
